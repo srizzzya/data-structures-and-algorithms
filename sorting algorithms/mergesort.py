fd3e6dc4e134c0c9702f37ed8c0028265a22495d
srizzzya <sriyaasrikanth@gmail.com>
2024-10-24 18:55:48 +0530

    Add files via upload

---
+++ sorting algorithms/mergesort.py
@@ -0,0 +1,34 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    leftHalf = arr[:mid]
+    rightHalf = arr[mid:]
+
+    sortedLeft = mergeSort(leftHalf)
+    sortedRight = mergeSort(rightHalf)
+
+    return merge(sortedLeft, sortedRight)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+unsortedArr = [11,123,88,34,56,78,90]
+sortedArr = mergeSort(unsortedArr)
+print("Sorted array:", sortedArr)
+
